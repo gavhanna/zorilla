07733fa15ed000f2b2e187093fead8d9af4ee041
Gavin Hanna <gavhanna@gmail.com>
2025-12-26 15:22:01 +0000

    Stripped back tanstack demo stuff from header and root index file

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -1,18 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {
-  Zap,
-  Server,
-  Route as RouteIcon,
-  Shield,
-  Waves,
-  Sparkles,
-} from 'lucide-react'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
+    <div className="">
     </div>
   )
 }
