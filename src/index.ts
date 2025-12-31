7b5853c473f05a417102cf801dbcaebca41459bd
Gavin Hanna <gavhanna@gmail.com>
2025-12-31 20:57:14 +0000

    Remove client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import userRoutes from "./routes/userRoutes";
 import recordingRoutes from "./routes/recordingRoutes";
 import authRoutes from "./routes/auth.routes";
@@ -12,16 +11,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/recordings", recordingRoutes);
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(process.cwd(), "client/.output/public")));
-
-  import("../client/.output/server/index.mjs").then((mod) => {
-    app.use(mod.middleware);
-  });
-} else {
-  console.log("Running in development mode: Client middleware skipped. Use Vite dev server.");
-}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
