7399b5b9701308c660fd1100f0f7acaaf6411fc6
Gavin Hanna <gavhanna@gmail.com>
2026-01-02 09:22:14 +0000

    Added morgan loggng

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import userRoutes from "./routes/userRoutes";
 import recordingRoutes from "./routes/recordingRoutes";
 import authRoutes from "./routes/auth.routes";
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/recordings", recordingRoutes);
